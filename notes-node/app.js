944af821cebfb24b619359fc9f93c3664eac9763
Łukasz <solka.lukasz@gmail.com>
2017-06-29 20:07:47 +0200

    3. ways to require modules. basics of lodash.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,8 +1,28 @@
-console.log('Starting app');
+console.log('Starting app.js');
 //https://nodejs.org/api/ - list of node modules.
+
+
+//require build in modules
 const fs = require('fs');
 const os = require('os');
 
-let user = os.userInfo();
 
-fs.appendFile('greatings.txt', `Hello ${user.username}!`);
+// require written modules
+const notes = require('./notes.js');
+
+
+//require 3rd party modules
+const _ = require('lodash');
+
+let user = os.userInfo();
+let res = notes.addNote();
+
+console.log(_.isString(true));
+console.log(_.isString('Lukas'));
+
+let filteredArray = _.uniq([1,1,1,2,3,4,4,3,3,2]);
+
+console.log(filteredArray);
+console.log('Result: ', notes.add(5,7));
+
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
---
+++ notes-node/notes.js
@@ -0,0 +1,11 @@
+console.log('Starting notes.js');
+
+module.exports.addNote = () => {
+    "use strict";
+    console.log('addNote');
+    return 'New Note';
+};
+module.exports.add = (a,b) => {
+    "use strict";
+    return a + b;
+};
