b7154185b096f292f860ad5ef863a58b51b89ded
Łukasz <solka.lukasz@gmail.com>
2017-07-01 17:16:57 +0200

    Install Yargs module, simple examples in usage

--- notes/app.js
+++ notes/app.js
@@ -2,10 +2,28 @@ console.log('Starting app.js');
 //https://nodejs.org/api/ - list of node modules.
 
 //require build in modules
-const fs = require('fs');
+const fs = require('fs'); // fileSystem
 //require 3rd party modules
-const _ = require('lodash');
-
+const _ = require('lodash'); // Helpers
+const yargs = require('yargs'); // Simplified input
 // require written modules
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
+//passing args to app: node app.js list // list is an arg
+let command = process.argv[2];
+console.log('Command: ', command);
+// console.log('Process: ', process.argv);
+console.log('------------------------------------');
+console.log('Yargs: ', argv);
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === "read") {
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNotes(argv.title);
+} else {
+    console.log('Command not recognized');
+}
