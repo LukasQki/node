2870091c9499d6097b652774f58c5ddb24a5b986
Łukasz <solka.lukasz@gmail.com>
2017-07-01 17:17:17 +0200

    Install Yargs module, simple examples in usage

--- notes/notes.js
+++ notes/notes.js
@@ -1,7 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
+let addNote = (title, body) => {
     "use strict";
-    console.log('addNote');
-    return 'New Note';
+    console.log('Adding note', title, body);
+};
+let getAll = () => {
+    "use strict";
+    console.log('Getting all notes');
+};
+let getNote = (title) => {
+    "use strict";
+    console.log('Reading note:', title);
+};
+let removeNotes = (title) => {
+    "use strict";
+    console.log('Removing note:', title);
+};
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNotes
 };
