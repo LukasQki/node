d61cbcda501c64114ba8026e6ab861b8d856d1f9
Łukasz <solka.lukasz@gmail.com>
2017-07-05 20:06:02 +0200

    Grab the address, lat and lng from json

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,11 @@
 const request = require('request');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1%20plac%20defilad%20warszawa',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+
 });
