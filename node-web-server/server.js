0de479207dfa9990c4b21e4691fd909d0df78ed1
Łukasz <solka.lukasz@gmail.com>
2017-07-11 22:41:02 +0200

    Lets make some Middleware.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -27,6 +45,13 @@ app.get('/about', (req, res) => {
        pageTitle: 'About Page'
    });
 });
+
+// app.get('/maintments', (req, res) => {
+//     res.render('maintments.hbs', {
+//         pageTitle: 'Maintments Page',
+//         welcomeMessage: 'Sorry we are fixing things. See You soon'
+//     });
+// });
 
 app.get('/bad', (req, res) => {
     res.send({
