2209f4b16187fa95577b3fd0701da323bca0261c
Łukasz <solka.lukasz@gmail.com>
2017-07-13 22:01:44 +0200

    Lets make test looks easier. Assertion Library

--- node-test/utils/utils.test.js
+++ node-test/utils/utils.test.js
@@ -1,17 +1,44 @@
 const utils = require('./utils');
+const expect = require('expect');
+
 
 it('should add two numbers', () => {
     let res = utils.add(33,11);
-
-    if (res !== 44) {
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
+    expect(res).toBe(44)
+    .toBeA('number');
+    // if (res !== 44) {
+    //     throw new Error(`Expected 44, but got ${res}.`);
+    // }
 });
 
 it('should square a number.', () => {
     let res = utils.square(3);
-
-    if (res !== 9) {
-        throw new Error(`Expected 9, but got ${res}`);
-    }
+    expect(res).toBe(9)
+    .toBeA('number');
+    // if (res !== 9) {
+    //     throw new Error(`Expected 9, but got ${res}`);
+    // }
 });
+
+it('should verify first and last name are set', () => {
+    let user = {location: 'Warsaw', age: 28};
+    let res = utils.setName(user, 'Lukas Solka');
+    expect(res).toInclude({
+        firstName: 'Lukas',
+        lastName: 'Solka'
+    }).toBeA('object');
+
+});
+
+// it('should expect some values', () => {
+    // expect(12).toNotBe(11);
+    // expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
+    // expect([2,3,4]).toExclude(1);
+    // expect({
+    //     name: 'Lukas',
+    //     age: 28,
+    //     location: 'Warsaw'
+    // }).toInclude({
+    //     age: 28
+    // });
+// });
