7dcd056483ac14964360211959eb552b116e18a6
Łukasz <solka.lukasz@gmail.com>
2017-07-15 23:27:33 +0200

    Make tests more readable. Using describe()

--- node-test/utils/utils.test.js
+++ node-test/utils/utils.test.js
@@ -1,37 +1,41 @@
 const utils = require('./utils');
 const expect = require('expect');
 
+describe('utils', () => {
 
-it('should add two numbers', () => {
-    let res = utils.add(33,11);
-    expect(res).toBe(44)
-    .toBeA('number');
-    // if (res !== 44) {
-    //     throw new Error(`Expected 44, but got ${res}.`);
-    // }
-});
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            let res = utils.add(33,11);
+            expect(res).toBe(44)
+                .toBeA('number');
+            // if (res !== 44) {
+            //     throw new Error(`Expected 44, but got ${res}.`);
+            // }
+        });
 
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(3,4, (sum) => {
-      expect(sum).toBe(7).toBeA('number');
-      done();
-    })
-});
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(3,4, (sum) => {
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            })
+        });
+    });
 
-it('should square a number.', () => {
-    let res = utils.square(3);
-    expect(res).toBe(9)
-    .toBeA('number');
-    // if (res !== 9) {
-    //     throw new Error(`Expected 9, but got ${res}`);
-    // }
-});
+    it('should square a number.', () => {
+        let res = utils.square(3);
+        expect(res).toBe(9)
+            .toBeA('number');
+        // if (res !== 9) {
+        //     throw new Error(`Expected 9, but got ${res}`);
+        // }
+    });
 
-it('should async square a number', (done) => {
-   utils.asyncSquare(3, (square) => {
-       expect(square).toBe(9).toBeA('number');
-       done();
-   })
+    it('should async square a number', (done) => {
+        utils.asyncSquare(3, (square) => {
+            expect(square).toBe(9).toBeA('number');
+            done();
+        })
+    });
 });
 
 it('should verify first and last name are set', () => {
@@ -43,6 +47,7 @@ it('should verify first and last name are set', () => {
     }).toBeA('object');
 
 });
+
 
 // it('should expect some values', () => {
     // expect(12).toNotBe(11);
